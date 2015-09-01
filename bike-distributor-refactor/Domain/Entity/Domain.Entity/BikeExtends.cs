8a6fe37e806841f466fa34e588516c8e5f681039
domingos.paola <domingos.paola@ecosistemas.com.br>
2015-09-01 18:17:26 -0300

    ok

---
+++ bike-distributor-refactor/Domain/Entity/Domain.Entity/BikeExtends.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infra.Common.Enum;
+
+namespace Domain.Entity
+{
+    public partial class Bike
+    {
+        public NumericValues PriceRage 
+        {
+            get 
+            {
+                if (this.Price >= (int)NumericValues.OneThousand && this.Price < (int)NumericValues.TwoThousand) 
+                {
+                    return NumericValues.OneThousand;
+                }
+                else if (this.Price >= (int)NumericValues.TwoThousand && this.Price < (int)NumericValues.FiveThousand) 
+                {
+                    return NumericValues.TwoThousand;
+                }
+                else if (this.Price >= (int)NumericValues.FiveThousand)
+                {
+                    return NumericValues.FiveThousand;
+                }
+                else 
+                {
+                    return NumericValues.NoDiscount;
+                }
+            }
+        }
+    }
+}
